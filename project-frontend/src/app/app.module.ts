e9ce4a472de6a4c0640b1677122c53273eef27d7
Marcos Arteta <marcos@claimpay.net>
2024-06-20 17:01:15 -0500

    Dasdasd

--- project-frontend/src/app/app.module.ts
+++ project-frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ListaComponent } from './components/actividades/lista/lista.component';
+import { DetalleComponent } from './components/actividades/detalle/detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
